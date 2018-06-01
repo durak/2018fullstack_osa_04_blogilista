db371345a2c988953df6db5ff57fb469dd78c0cc
durak <itapkos@gmail.com>
2018-06-01 15:36:05 +0300

    h4_04

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper')
+const dummyBlogs = require('./blogs').blogs
 
 test('dummy is called', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [dummyBlogs[0]]
+  const listWithManyBlogs = dummyBlogs
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(7)
+  })
+
+  test('of list with many blogs is calculated correctly', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(36)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,13 @@ const dummy = (blogs) => {
   return use
 }
 
+const totalLikes = (blogs) => {  
+  const reducer = (likesSum, blog) => likesSum + blog.likes
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
