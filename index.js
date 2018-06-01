3fc5ef08ea9a695d9ab68ec58b644e20397ac68c
durak <itapkos@gmail.com>
2018-06-01 03:03:40 +0300

    h4_2 refaktorointi

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -32,23 +25,8 @@ mongoose
     console.log(err)
   })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// routemäärittelyt
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
