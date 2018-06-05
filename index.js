af24c3d8353d82386d42b749c704273da4055fe7
durak <itapkos@gmail.com>
2018-06-05 13:11:42 +0300

    h4_15

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 
 
@@ -26,6 +27,7 @@ app.use(bodyParser.json())
 
 // routemäärittelyt
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 // error middleware
 
